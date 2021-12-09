5ac0fa2feec6b967509fe25c7d2a4ba2bfda5e7f
bgtheodore18@gmail.com <bgtheodore18@gmail.com>
2021-12-09 23:26:25 +0100

    After API break

--- grails-app/controllers/fr/fullstack/project/ApiController.groovy
+++ grails-app/controllers/fr/fullstack/project/ApiController.groovy
@@ -58,12 +58,49 @@ class ApiController {
 //    Utilisateur singleton
     def user()
     {
-
+        switch (request.getMethod())
+        {
+            case "GET":
+//                1. Verifier si l'ID est bien fourni
+                if (!params.id)
+                    return response.status = 400
+//                2. Changer l'instance correspondante et verifier qu'elle existe
+                def userInstance = User.get(params.id)
+                if (!userInstance)
+                    return response.status = 404
+//                3. Serialiser l'instance recuperee a l'appelant
+                response.withFormat {
+                    xml { render userInstance as XML}
+                    json { render userInstance as JSON}
+                }
+                break;
+            case "PUT":
+                break;
+            case "PATCH":
+                break;
+            case "DELETE":
+                break;
+            default:
+                return response.status = 405
+                break
+        }
+        return response.status = 406
     }
 
 //    Liste d'utilisateurs
     def users()
     {
-
+        switch (request.getMethod())
+        {
+            case "GET":
+                def userList = User.list()
+                response.withFormat {
+                    xml { render userList as XML}
+                    json { render userList as JSON}
+                }
+                break;
+            case "POST":
+                break;
+        }
     }
 }
